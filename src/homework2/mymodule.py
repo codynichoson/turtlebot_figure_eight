dcbd2683f09b658f9a4de6fbcd84fdf08ba51a16
Cody Nichoson <codynichoson@gmail.com>
2021-10-15 17:17:22 -0500

    Trajectory calcs moved to Python package

---
+++ src/homework2/mymodule.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sympy as sym
+
+class trajectory():
+    def __init__(self, W, H, T):
+        self.t = sym.symbols('t')
+
+        x = (W/2)*sym.sin(2*sym.pi*self.t/T)
+        self.xdot = x.diff(self.t)
+
+        y = (H/2)*sym.sin(4*sym.pi*self.t/T)
+        self.ydot = y.diff(self.t)
+
+    def lin_vel(self, time):
+        v = sym.sqrt(self.xdot**2 + self.ydot**2)
+        lin_vel_func = sym.lambdify(self.t,v)
+        return lin_vel_func(time)
+
+    def ang_vel(self, time):
+        theta = sym.atan(self.ydot/self.xdot)
+        thetadot = theta.diff(self.t)
+        ang_vel_func = sym.lambdify(self.t,thetadot)
+        return ang_vel_func(time)
+
