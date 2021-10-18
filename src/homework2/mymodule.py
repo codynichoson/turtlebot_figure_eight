6e05cd3a7bf3159df0e6eab54a0f8161c85b234f
Cody Nichoson <codynichoson@gmail.com>
2021-10-18 17:12:16 -0500

    Unittest successfully working

--- src/homework2/mymodule.py
+++ src/homework2/mymodule.py
@@ -3,6 +3,7 @@
 import numpy as np
 import sympy as sym
 
+
 class trajectory():
     def __init__(self, W, H, T):
         self.t = sym.symbols('t')
@@ -32,3 +33,30 @@ class trajectory():
         theta0 = ang_func(0)
         return theta0
 
+    def x_test(self):
+        x_func = sym.lambdify(self.t, self.x)
+        return x_func(0)
+
+    def y_test(self):
+        y_func = sym.lambdify(self.t, self.y)
+        return y_func(0)
+
+    def xdot_test(self):
+        xdot_func = sym.lambdify(self.t, self.xdot)
+        return xdot_func(0)
+
+    def ydot_test(self):
+        ydot_func = sym.lambdify(self.t, self.ydot)
+        return ydot_func(0)
+
+    def xddot_test(self):
+        xddot_func = sym.lambdify(self.t, self.xddot)
+        return xddot_func(0)
+
+    def yddot_test(self):
+        yddot_func = sym.lambdify(self.t, self.yddot)
+        return yddot_func(0)
+
+if __name__ == "__main__":
+    traj = trajectory(1,1,10)
+    print(traj.x_test())
--- test/hw2_test.py
+++ test/hw2_test.py
@@ -1,10 +1,39 @@
 import unittest
 import homework2.mymodule
+import sympy as sym
+from numpy import pi
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEquals(2, 2)
+W = 1
+H = 1
+T = 10
+traj = homework2.mymodule.trajectory(W,H,T)
+
+class GettingTesty(unittest.TestCase):
+    
+    def test_x(self):
+        global traj
+        self.assertEquals(traj.x_test(), 0)
+
+    def test_y(self):
+        global traj
+        self.assertEquals(traj.y_test(), 0)
+    
+    def test_xdot(self):
+        global traj
+        self.assertEquals(traj.xdot_test(), pi/10)
+    
+    def test_ydot(self):
+        global traj
+        self.assertEquals(traj.ydot_test(), pi/5)
+
+    def test_xddot(self):
+        global traj
+        self.assertEquals(traj.xddot_test(), 0)
+
+    def test_yddot(self):
+        global traj
+        self.assertEquals(traj.yddot_test(), 0)
 
 if __name__ == "__main__":
     import rosunit
-    rosunit.unitrun(homework2, 'test_class_name', MyTestCase)
+    rosunit.unitrun(homework2, 'test_class_name', GettingTesty)
